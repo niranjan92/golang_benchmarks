4616cbd7428682d81eb696dcf95f9c952e31cb5c
Niranjan Godbole <niranjan92@users.noreply.github.com>
2017-05-21 02:38:19 +0530

    Update runBenchmark.sh

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -3,9 +3,14 @@ echo "starting benchmarks..."
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 source $HOME/.gvm/scripts/gvm
 
+#Bootstrap go for compilation
+gvm install go1.8.1 -B
+gvm use go1.8.1
+
 gvm install master
 gvm use master
 
 go version 
 
+cat /root/.gvm/logs/go-master-compile.log
 
