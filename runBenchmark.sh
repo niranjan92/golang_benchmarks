023d34a889365c532b93ec633de3dd0a0a118d0f
Niranjan Godbole <niranjan92@users.noreply.github.com>
2017-05-21 03:18:44 +0530

    Update runBenchmark.sh

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -1,5 +1,9 @@
 echo "starting benchmarks..."
 
+#to get perf tool
+kernel=$(uname -r)
+apt-get install linux-tools-common linux-tools-generic linux-tools-$kernel
+
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 source $HOME/.gvm/scripts/gvm
 
@@ -8,9 +12,14 @@ gvm install go1.8.1 -B
 gvm use go1.8.1
 
 gvm install master
+cat /root/.gvm/logs/go-master-compile.log
 gvm use master
-
 go version 
 
-cat /root/.gvm/logs/go-master-compile.log
+echo "building benchmarks in $PWD/go_bench_bin. where $PWD="
+echo $PWD
+mkdir $PWD/go_bench_bin
+GOBIN=$PWD/go_bench_bin go get golang.org/x/benchmarks/...
 
+echo "running benchmarks"
+$PWD/go_bench_bin/build -benchnum 5
