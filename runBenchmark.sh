52880d31397251d5fd01889e322027d89d98f647
Niranjan Godbole <niranjan92@users.noreply.github.com>
2017-05-21 02:33:20 +0530

    Update runBenchmark.sh

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -1,1 +1,11 @@
-echo "run benchmark success!!!"
+echo "starting benchmarks..."
+
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+source $HOME/.gvm/scripts/gvm
+
+gvm install master
+gvm use master
+
+go version 
+
+
